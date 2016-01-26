992376837abc6fcbe29d1966fb481fc79b093334
luthfiapriyanto <luthfiapriyanto@gmail.com>
2016-01-26 13:01:20 +0700

    try

---
+++ app/src/main/java/com/example/luthf/pointofsale/EmployeeLogin.java
@@ -0,0 +1,13 @@
+package com.example.luthf.pointofsale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_login);
+    }
+}
